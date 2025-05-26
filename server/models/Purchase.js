c242a6746707ef7ba0fee812ddd493419e9f105e
rishi1907 <risikeshranjan074@gmail.com>
2025-05-26 13:16:05 +0530

    some changes

--- server/models/Purchase.js
+++ server/models/Purchase.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
-const purchaseSchema = new mongoose.Schema({
-  userId: {
-    type: String, // Clerk user ID
-    required: true,
-  },
-  courseId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "completed", "failed"],
-    default: "pending",
-  },
+const PurchaseSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        ref: "User",
+        required: true,
+    },
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "completed", "failed"],
+        default: "pending",
+    },
 }, {
-  timestamps: true
+    timestamps: true
 });
 
-export const Purchase = mongoose.model("Purchase", purchaseSchema);
+export const Purchase = mongoose.model("Purchase", PurchaseSchema);
